7dd194035b2b6787af346dc6a9e2e24c4e56db58
tugrulguner <tugrulgunr@gmail.com>
2022-02-03 18:12:07 -0500

    train.py updated

--- train.py
+++ train.py
@@ -51,17 +51,13 @@ def main():
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    # TODO: Create TabularDataset using TabularDatasetFactory
-    # Data is located at:
-    # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-    ds = ### YOUR CODE HERE ###
+    ds = TabularDatasetFactory.from_delimited_files(
+        path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    )
     
     x, y = clean_data(ds)
 
-    # TODO: Split data into train and test sets.
-
-    ### YOUR CODE HERE ###a
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
